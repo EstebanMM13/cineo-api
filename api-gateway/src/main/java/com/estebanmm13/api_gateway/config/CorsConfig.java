f06c6cfe9dead9014eecf744bffc5bae859ee511
EstebanMM13 <2001estebanmartinez@gmail.com>
2026-07-06 16:54:15 +0200

    Agregar seguridad con parametros de entorno 2

--- api-gateway/src/main/java/com/estebanmm13/api_gateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/estebanmm13/api_gateway/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.estebanmm13.api_gateway.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -8,17 +9,21 @@ import org.springframework.web.filter.CorsFilter;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
 public class CorsConfig {
+
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
 
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:4200", "http://192.168.1.55:4200"));
+        config.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         config.setExposedHeaders(List.of("Authorization"));
